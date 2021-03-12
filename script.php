df26e0975ac898e007e727ec3bf4ba5b87fecdcb
ulisse-fontanot <ulissef99@gmail.com>
2021-03-12 17:57:13 +0100

    aggiunto sconto non funziona

--- script.php
+++ script.php
@@ -12,6 +12,7 @@ class Movie {
     public $regista;
     public $genere;
     public $prezzo;
+    private $sconto = 25;
 
     public function __construct($_titolo,$_regista,$_genere,$_prezzo){
         $this->titolo = $_titolo;
@@ -20,12 +21,24 @@ class Movie {
         $this->prezzo = $_prezzo;
     }
 
+    public function setSconto($_sconto){
+        if ($_genere = 'fantascienza') {
+            $this->sconto = $_prezzo - (($_prezzo / 100) * $_sconto);
+        }
+    }
+
+    public function getSconto(){
+        return $this->sconto;
+    }
+
 }
 
-$movie1 = new Movie('STAR WARS','George Lucas','fantascienza','8,00 €');
+$movie1 = new Movie('STAR WARS','George Lucas','fantascienza',8);
 var_dump($movie1);
 
-$movie2 = new Movie('THE HATEFUL EIGHT','Quentin Tarantino','storico','10,00 €');
+$movie2 = new Movie('THE HATEFUL EIGHT','Quentin Tarantino','storico',10);
 var_dump($movie2);
+
+
 
 ?>
